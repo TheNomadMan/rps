296b890a2601659b925617e6c6063ca0b5e0f9a1
TheNomadMan <thenomadman@hotmail.com>
2025-11-11 07:06:05 -0500

    Formatting upgrades

--- rps.py
+++ rps.py
@@ -6,31 +6,32 @@ while True:
     while True:
         user_choice = str(input("'Rock', 'Paper', or 'Scissors'?\n")).lower()
         if user_choice in valid_choices:
-            print("You picked:", user_choice)
+            print()
+            print("You picked:", user_choice.capitalize())
             break
         else:
             print("That's not a valid choice, try again")
 
     computer_pick = random.choice(valid_choices)
-    print("The computer chose:", computer_pick)
-
+    print("The computer chose:", computer_pick.capitalize())
+    print()
     if computer_pick == user_choice:
         print("Its a tie!")
     elif computer_pick == "rock" and user_choice == "scissors":
-        print("Rock beats Scissors, you lose")
+        print("Rock beats Scissors: you lose.")
     elif computer_pick == "scissors" and user_choice == "paper":
-        print("Scissors beats Paper, you lose")
+        print("Scissors beats Paper: you lose.")
     elif computer_pick == "paper" and user_choice == "rock":
-        print("Paper beats Rock, you lose")
+        print("Paper beats Rock: you lose.")
     elif user_choice == "rock" and computer_pick == "scissors":
-        print("Rock beats Scissors, you win!")
+        print("Rock beats Scissors: you win!")
     elif user_choice == "scissors" and computer_pick == "paper":
-        print("Scissors beats Paper, you win!")
+        print("Scissors beats Paper: you win!")
     elif user_choice == "paper" and computer_pick == "rock":
-        print("Paper beats Rock, you win!")
+        print("Paper beats Rock: you win!")
     else:
         print("Something went wrong")
-
+    print()
     while True:
         print("Would you like to play again?")
         play_again = str(input("'Yes' or 'No'\n")).lower()
@@ -39,9 +40,11 @@ while True:
                 print("Goodbye!")
                 break
             elif play_again == "yes":
-                print("Yay!")
+                print()
+                print("Yay!\n")
                 break
         else:
-            print("Huh?")
+            print()
+            print("Huh?\n")
     if play_again == "no":
         break
